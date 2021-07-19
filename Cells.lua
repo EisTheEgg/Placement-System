2cf552bcade5ff1aa6f00f62d10fcd3cdaaa6c43
Eis <80625199+EisTheEgg@users.noreply.github.com>
2021-07-19 09:59:01 +0200

    Update Cells.lua
    Added more comments, took the plot into account and used math.round instead.

--- Cells.lua
+++ Cells.lua
@@ -11,10 +11,11 @@ local function RoundVector2(vector, multiple)
 	)
 end
 
-local function GetModelSize(Model)
-	local ModelSize = CFrame.fromEulerAnglesXYZ(0, math.rad(Model.PrimaryPart.Orientation.Y), 0) * Model.PrimaryPart.Size
+local function GetModelSize(Plot, Model)
+	-- Get size of model with orientation taken into account.
+	local ModelSize = CFrame.fromEulerAnglesXYZ(0, math.rad(Model.PrimaryPart.Orientation.Y - Plot.Orientation.Y), 0) * Model.PrimaryPart.Size
 	ModelSize = Vector3.new(math.abs(ModelSize.X), math.abs(ModelSize.Y), math.abs(ModelSize.Z))
-	ModelSize = Vector3.new(math.floor(ModelSize.X + 0.5), 0, math.floor(ModelSize.Z + 0.5))
+	ModelSize = Vector3.new(math.round(ModelSize.X), 0, math.round(ModelSize.Z))
 	
 	return ModelSize
 end
@@ -32,12 +33,13 @@ function Grid:translate(x, y)
 	return Vector.X, Vector.Y
 end
 
-function Grid:_GetModelCells(Model, x, y, Callback)
-	local ModelSize = GetModelSize(Model)
+function Grid:_GetModelCells(Plot, Model, x, y, Callback)
+	-- Gets all the cells that the model covers. (8x4 will cover two cells.)
+	local ModelSize = GetModelSize(Plot, Model)
 	local GridUnit = self.GridUnit
 	
 	-- Get top corner of model
-	x = math.floor(x + 0.5) - ModelSize.X / 2; y = math.floor(y + 0.5) - ModelSize.Z / 2
+	x = math.round(x) - ModelSize.X / 2; y = math.round(y) - ModelSize.Z / 2
 	
 	for i = 1, ModelSize.X / GridUnit do
 		local x = x + (GridUnit / 2 + (i - 1) * GridUnit)
@@ -46,18 +48,17 @@ function Grid:_GetModelCells(Model, x, y, Callback)
 		-- x = x + 2
 		
 		for j = 1, ModelSize.Z / GridUnit do
-			local y = y + (GridUnit / 2 + (j - 1) * GridUnit)
-			local x, y = self:translate(x, y)
+			local x, y = self:translate(x, y + (GridUnit / 2 + (j - 1) * GridUnit))
 			
 			Callback(x, y)
 		end
 	end
 end
 
-function Grid:GetCell(Model, x, y)
+function Grid:GetCell(Plot, Model, x, y)
 	local CellHasObject = false
 	
-	self:_GetModelCells(Model, x, y, function(x, y)
+	self:_GetModelCells(Plot, Model, x, y, function(x, y)
 		if self.Grid[x] and self.Grid[x][y] then
 			CellHasObject = true
 		end
@@ -66,15 +67,15 @@ function Grid:GetCell(Model, x, y)
 	return CellHasObject
 end
 
-function Grid:AddCell(Model, x, y)
-	self:_GetModelCells(Model, x, y, function(x, y)
+function Grid:AddCell(Plot, Model, x, y)
+	self:_GetModelCells(Plot, Model, x, y, function(x, y)
 		self.Grid[x] = self.Grid[x] or {}
 		self.Grid[x][y]= true
 	end)
 end
 
-function Grid:RemoveCell(Model, x, y)	
-	self:_GetModelCells(Model, x, y, function(x, y)
+function Grid:RemoveCell(Plot, Model, x, y)
+	self:_GetModelCells(Plot, Model, x, y, function(x, y)
 		self.Grid[x][y] = nil
 	end)
 end
